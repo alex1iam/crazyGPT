14ea3ae94523ea951840bebef3a1812a5695caea
alex1iam <61625470+alex1iam@users.noreply.github.com>
2024-08-25 01:23:26 +1000

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,33 @@
+dict_equipment = {
+    "input/light_z0_on_off": {'name': 'свет', 'location': 'милайт'},
+    "input/light_z2_on_off": {'name': 'свет', 'location': 'зал'},
+    "zigbee2mqtt/5e24. КУХНЯ Лампа/set": {'name': 'лампа', 'location': 'кухня'},
+    "input/light_z3_on_off": {'name': 'люстра', 'location': 'зал'},
+    "input/light_z4_on_off": {'name': 'свет', 'location': 'кухня'},
+    "input/light_z1_on_off": {'name': 'свет', 'location': 'прихожая'},
+    "zigbee2mqtt/9665. ВАННАЯ Свет/set": {'name': 'свет', 'location': 'ванная'},
+    "zigbee2mqtt/8acc. НИША Свет/set": {'name': 'свет', 'location': 'ниша'},
+    "zigbee2mqtt/cfa4. ЗАЛ Штора справа/set": {'name': 'штора справа', 'location': 'зал'},
+    "zigbee2mqtt/d38e. ЗАЛ Штора в центре/set": {'name': 'штора центр', 'location': 'зал'},
+    "zigbee2mqtt/8e8a. ЗАЛ Штора слева/set": {'name': 'штора слева', 'location': 'зал'},
+    "zigbee2mqtt/29c6. КУХНЯ Штора справа/set": {'name': 'штора справа', 'location': 'кухня'},
+    "zigbee2mqtt/4d83. КУХНЯ Штора слева/set": {'name': 'штора слева', 'location': 'кухня'},
+    "zigbee2mqtt/639b. ВАННАЯ Клапан горячей воды/set": {'name': 'горячая вода', 'location': 'ванная'},
+    "zigbee2mqtt/37cf. ВАННАЯ Клапан холодной воды/set": {'name': 'холодная вода', 'location': 'ванная'},
+    "zigbee2mqtt/e4ee. ЗАЛ Умная розетка/set": {'name': 'розетка', 'location': 'зал'},
+    "zigbee2mqtt/e945. ЗАЛ Теплый пол/set": {'name': 'пол', 'location': 'зал'}
+}
+
+dict_action = {
+    "включи": ['on'],
+    "открой": ['open'],
+    "выключи": ['off'],
+    "закрой": ['close'],
+    "громче": ['+1'],
+    "выше": ['+1'],
+    "следующий": ['+1'],
+    "тише": ['-1'],
+    "ниже": ['-1'],
+    "прежний": ['-1'],
+    "предыдущий": ['-1']
+}
